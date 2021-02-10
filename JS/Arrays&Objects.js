7836c521cd33d3202102d7d68285d3cd86e96adc
Mubashir Jan <janmubashir300@gmail.com>
2021-02-10 17:48:19 -0500

    7th revision

--- JS/Arrays&Objects.js
+++ JS/Arrays&Objects.js
@@ -16,12 +16,14 @@ const arr = new Array(23, 55, 34, 'Pineapple');
 2. Methods
 */
 
-//PROPERTIES
-const arr1 = new Array(1, 2, 3, 'Avacado');
+//PROPERTIES & METHODS
+//DO NOT DECLARE STRING, NUMBERS AND BOOLEAN AS OBJECTS
+// ========================================================
+const arr1 = new Array(1, 2, 3, 'Avacado'); //using 'new' will convert the array into an array object
 // console.log(arr1.length); // GIVES THE LENGTH OF AN ARRAY - 4
 // console.log(arr1);
-//to check whether a particular list is an array or not
 
+//to check whether a particular list is an array or not
 // console.log(Array.isArray(arr1)); // IS ARRAY - GIVES THE VALUE TRUE
 // console.log(Array.isArray('dfdfsf')); // NOT ARRAY - GIVES THE VALUE FALSE
 
@@ -29,9 +31,32 @@ const arr1 = new Array(1, 2, 3, 'Avacado');
 arr1[3] = 'Papaya';
 // console.log(arr1);
 
-//TO SHOW A PARTICULAR OBJECT IN AN ARRAY
+//TO SHOW A PARTICULAR ELEMENT IN AN ARRAY
 let arrayy = arr1[0];
 // console.log('the element is :', arrayy);
 
-let marksValue = marks.indexOf(87);
-console.log(marksValue);
+//getting an index of an array
+let marksValue = marks.indexOf(87); 
+// console.log(marksValue);
+
+// to insert an element at END of array
+marks.push(6455);
+// console.log(marks);
+
+//to insert an element at the BEGINNING of an array
+marks.unshift('ladyFinger', [34, 23]);
+// console.log(marks);
+
+// to remove an element from the end of an array
+marks.pop();
+// console.log(marks);
+
+//to remove an element from the start of an array
+marks.shift();
+// console.log(marks);
+
+// to remove an specific elements from an array
+marks.splice(1,2); // this method start removing elements from 1 and remove two additional 
+console.log(marks);
+
+

14027d22de97557cef62aee80fcb130da2464c74
Mubashir Jan <janmubashir300@gmail.com>
2021-02-10 15:51:48 -0500

    6th revision

--- JS/Arrays&Objects.js
+++ JS/Arrays&Objects.js
@@ -17,14 +17,21 @@ const arr = new Array(23, 55, 34, 'Pineapple');
 */
 
 //PROPERTIES
-let arr1 = new Array(1, 2, 3, 'Avacado');
-console.log(arr1.length); // GIVES THE LENGTH OF AN ARRAY - 4
-
+const arr1 = new Array(1, 2, 3, 'Avacado');
+// console.log(arr1.length); // GIVES THE LENGTH OF AN ARRAY - 4
+// console.log(arr1);
 //to check whether a particular list is an array or not
 
-console.log(Array.isArray(arr1)); // IS ARRAY - GIVES THE VALUE TRUE
-console.log(Array.isArray('dfdfsf')); // NOT ARRAY - GIVES THE VALUE FALSE
+// console.log(Array.isArray(arr1)); // IS ARRAY - GIVES THE VALUE TRUE
+// console.log(Array.isArray('dfdfsf')); // NOT ARRAY - GIVES THE VALUE FALSE
 
 //TO CHANGE A PARTICULAR OBJECT IN AN ARRAY
 arr1[3] = 'Papaya';
 // console.log(arr1);
+
+//TO SHOW A PARTICULAR OBJECT IN AN ARRAY
+let arrayy = arr1[0];
+// console.log('the element is :', arrayy);
+
+let marksValue = marks.indexOf(87);
+console.log(marksValue);

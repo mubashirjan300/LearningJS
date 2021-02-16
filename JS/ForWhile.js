e460f9f0c386ed11a3b3417f8cef2f626b2fdec1
Mubashir Jan <janmubashir300@gmail.com>
2021-02-16 13:32:52 -0500

    11th Rev

---
+++ JS/ForWhile.js
@@ -0,0 +1,22 @@
+// GENERAL LOOPS: for, while, do-while
+
+for(let i=0; i<50; i++) //first it will assign 0 to i, then it will check the condition i<50; and when the condition is true, it will print i; and lastly it will increment the value of i with 1
+{
+    // console.log(i)
+}
+
+// ++ OPERATOR MEANS ADD 1
+
+let a=34;
+a++; // or we can write this as a+=1
+// console.log(a);
+
+
+// WHILE LOOP:
+
+let j = 0;
+while(j<10){
+    // console.log(j);
+    console.log(j+1) // if we want the value to be printed from 1 and not 0
+    j++;
+}
